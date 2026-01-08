3b5c60cbccc4ac3974304b84016d1e84e09dd4e9
mndesystems-ship-it <mndesystems@gmail.com>
2026-01-08 04:07:50 -0500

    Add OrbitErrorCode enum for error handling

---
+++ orbit-core/src/main/java/org/mnde/orbit/OrbitErrorCode.java
@@ -0,0 +1,11 @@
+package org.mnde.orbit;
+
+public enum OrbitErrorCode {
+    ERR_NOT_OBJECT,
+    ERR_ORBIT_VERSION,
+    ERR_ID_INVALID,
+    ERR_ACTION_INVALID,
+    ERR_ACTION_UNKNOWN,
+    ERR_PAYLOAD_INVALID,
+    ERR_OPEN_URL_REQUIRED
+}
